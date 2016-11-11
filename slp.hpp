96ac1813711dab1845670a8daf4279815720fd33
Ratan <ratagupt@in.ibm.com>
2016-11-11 19:12:42 +0530

    SLP Server
    This patch contains the entry point for the SLP
    and starts the SLP Server.
    
    Change-Id: I08d0f4675eeabe95127c43500a68c7819edc05be
    Signed-off-by: Ratan <ratagupt@in.ibm.com>

---
+++ slp.hpp
@@ -0,0 +1,316 @@
+#pragma once
+#include <stdio.h>
+#include <vector>
+#include <array>
+#include <memory>
+#include <string>
+#include <map>
+#include <list>
+#include "slp_service_info.hpp"
+
+namespace SLP
+{
+namespace REQUEST_TYPE //for all the request types
+{
+/** SLPSrvTypeRqst structure and associated functions */
+struct SrvType
+{
+
+    uint16_t prlistlen;
+    const char* prlist;
+    uint16_t namingauthlen;
+    const char* namingauth;
+    uint16_t scopelistlen;
+    const char* scopelist;
+
+    SrvType() :
+        prlistlen(0),
+        prlist(nullptr),
+        namingauthlen(0),
+        namingauth(nullptr),
+        scopelistlen(0),
+        scopelist(nullptr) {}
+
+    ~SrvType() = default;
+    SrvType(const SrvType&) = delete;
+    SrvType& operator=(const SrvType&) = delete;
+    SrvType(SrvType&&) = default;
+    SrvType& operator=(SrvType &&) = default;
+
+
+};
+struct Srv
+{
+    uint16_t prlistlen;
+    const char* prlist;
+    uint16_t srvtypelen;
+    const char* srvtype;
+    uint16_t scopelistlen;
+    const char* scopelist;
+    uint16_t predicatever;
+    uint16_t predicatelen;
+    const char* predicate;
+    uint16_t spistrlen;
+    const char* spistr;
+
+    Srv() :
+        prlistlen(0),
+        prlist(nullptr),
+        srvtypelen(0),
+        srvtype(nullptr),
+        scopelistlen(0),
+        scopelist(nullptr),
+        predicatelen(0),
+        predicate(nullptr),
+        spistrlen(0),
+        spistr(nullptr) {}
+
+    ~Srv() = default;
+    Srv(const Srv&) = delete;
+    Srv& operator=(const Srv&) = delete;
+    Srv(Srv&&) = default;
+    Srv& operator=(Srv &&) = default;
+
+};
+}
+
+enum class FunctionType : uint8_t
+{
+    SLP_FUNCT_SRVRQST     = 0x01,
+    SLP_FUNCT_SRVRPLY     = 0x02,
+    SLP_FUNCT_ATTRRQST    = 0x06,
+    SLP_FUNCT_ATTRRPLY    = 0x07,
+    SLP_FUNCT_SRVTYPERQST = 0x09,
+    SLP_FUNCT_SRVTYPERPLY = 0x0A,
+    SLP_FUNCT_SAADV       = 0x0B,
+};
+
+enum class Error : uint8_t
+{
+    LANGUAGE_NOT_SUPPORTED = 0x1,
+    PARSE_ERROR,
+    INVALID_REGISTRATION,
+    SCOPE_NOT_SUPPORTED,
+    AUTHENTICATION_UNKNOWN,
+    AUTHENTICATION_ABSENT,
+    AUTHENTICATION_FAILED,
+    VER_NOT_SUPPORTED = 0x9,
+    INTERNAL_ERROR,
+    DA_BUSY_NOW,
+    OPTION_NOT_UNDERSTOOD,
+    INVALID_UPDATE,
+    MSG_NOT_SUPPORTED,
+};
+
+
+struct Header
+{
+    uint8_t version;
+    uint8_t functionid;
+    std::array<uint8_t, 3> length;
+    uint16_t flags;
+    std::array<uint8_t, 3> extoffset;
+    uint16_t xid;
+    uint16_t langtaglen;
+    const char* langtag;
+
+    Header() :
+        version(0),
+        functionid(0),
+        flags(0),
+        xid(0),
+        langtaglen(0),
+        langtag(nullptr)
+    {
+
+        length.fill(0);
+        extoffset.fill(0);
+    }
+
+    ~Header() = default;
+    Header(const Header&) = delete;
+    Header& operator=(const Header&) = delete;
+    Header(Header&&) = default;
+    Header& operator=(Header &&) = default;
+
+};
+
+/** SLP wire protocol message management structures and prototypes */
+struct Message
+{
+    Header header;
+    union _body
+    {
+        REQUEST_TYPE::SrvType srvtyperqst;
+        REQUEST_TYPE::Srv srvrqst;
+        _body() {};
+        ~_body() {};
+
+    } body;
+
+    Message() = default;
+    ~Message() = default;
+    Message(const Message&) = delete;
+    Message& operator=(const Message&) = delete;
+    Message(Message&&) = default;
+    Message& operator=(Message &&) = default;
+};
+
+class Parser
+{
+    private:
+         //Not allowing it to instantiate
+         Parser() = default;
+        /** Parse header data from the buffer.
+         *
+         * @param[in] buffer - The buffer from which data should be parsed.
+         * @param[out] msg -   The msg obj into which buffer should be parsed
+         *
+         *
+         * @return Zero on success, or a non-zero error code.
+         *
+         * @internal
+         */
+
+        static uint8_t parseHeader(const std::vector<uint8_t>& buffer,
+                                   SLP::Message& msg);
+
+        /** This function will parse the request payload for a service type request.
+         *
+         * @param[in] buffer - The buffer from which data should be parsed.
+         * @param[out] msg - The msg obj  into which  buffer should be parsed.
+         *
+         * @return Zero on success, or a non-zero error code.
+         *
+         * @internal
+         */
+
+        static uint8_t parseSrvTypeRqst(const std::vector<uint8_t>& buffer,
+                                        SLP::Message& msg);
+
+        /** Parse a service request.
+          *
+          * @param[in] buffer - The buffer from which data should be parsed.
+          * @param[out] msg - The msg object into which buffer should be parsed.
+          *
+          * @return Zero on success, or a non-zero error code.
+          *
+          * @internal
+          */
+
+        static uint8_t parseSrvRqst(const std::vector<uint8_t>& buffer,
+                                    SLP::Message& msg);
+
+
+    public:
+        /** Parse a buffer and fill the header and the body of the message.
+         *
+         * @param[in] buffer - The buffer from which data should be parsed.
+         * @param[out] msg - The msg into which buffer should be parsed.
+         *
+         * @return Zero on success, or a non-zero error code.
+         *
+         */
+
+        static uint8_t parseBuffer(const std::vector<uint8_t>& buffer,
+                                   SLP::Message& msg);
+};
+
+class Handler
+{
+
+    public:
+
+        using SlpServiceList = std::map<std::string, SLP::ConfData>;
+
+        /** Handle the  request  message.
+         *
+         * @param[in] msg - The message to process
+         * @param[out] buff - The response buffer to fill.
+         *
+         * @return Zero on success, or a non-zero error code.
+         *
+         */
+
+        static uint8_t processRequest(const Message& msg,
+                                      std::vector<uint8_t>& buff);
+
+        /** Handle the error
+         *
+         * @param[in] msg - Req message
+         * @param[out] buff - The response buffer to fill.
+         * @param[in] err - Error code
+         *
+         */
+
+        static void processError(const Message& req,
+                                 std::vector<uint8_t>& buff,
+                                 uint8_t err);
+    private:
+         //Not allowing the class  to instantiate
+         Handler() = default;
+
+
+        /** Handle the  SrvRequest message.
+         *
+         * @param[in] msg - The message to process
+         * @param[out] buff - The response buffer to fill.
+         *
+         * @return Zero on success, or a non-zero error code.
+         *
+         */
+
+        static uint8_t processSrvRequest(const Message& msg,
+                                         std::vector<uint8_t>& buff);
+
+
+        /** Handle the  SrvTypeRequest message.
+         *
+         * @param[in] msg - The message to process
+         * @param[out] buff - The response buffer to fill.
+         *
+         * @return Zero on success, or a non-zero error code.
+         *
+         */
+
+        static uint8_t processSrvTypeRequest(const Message& msg,
+                                             std::vector<uint8_t>& buff);
+
+        /**  Read the SLPinfo from the configuration.
+         *
+         * @param[in] filename - Name of the conf file
+         * @param[out] svcList - Service List
+         *
+         * @return Zero on success, or a non-zero error code.
+         *
+         */
+        static uint8_t readSLPServiceInfo(const std::string& filename,
+                                          SlpServiceList& svcList);
+
+        /**  Read the SLPinfo from the configuration.
+         *
+         * @param[in] filename - Name of the conf file
+         * @param[out] svcList - Service List
+         *
+         * @return Zero on success, or a non-zero error code.
+         *
+         */
+
+        static uint8_t getIntfAddrs(std::list<std::string>& addrList);
+
+        /** Fill the buffer with the header data from the req object
+         *
+         * @param[in] req - Header data will be copied from
+         * @param[out] buff - data will be copied to.
+         *
+         */
+        static void prepareHeader(const Message& req,
+                                  std::vector<uint8_t>& buff);
+
+        static SlpServiceList slpSvcLst;
+
+};
+
+}
+
+
---
+++ slp_meta.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace SLP
+{
+    constexpr auto CONF_FILE = "/etc/slp_lite.conf";
+    constexpr size_t VERSION_2 = 2;
+    constexpr auto SUCCESS = 0;
+    namespace HEADER
+    {
+        constexpr size_t SIZE_VERSION = 1;
+        constexpr size_t SIZE_LENGTH = 3;
+        constexpr size_t SIZE_FLAGS = 2;
+        constexpr size_t SIZE_OFFSET = 3;
+        constexpr size_t SIZE_XID = 2;
+        constexpr size_t SIZE_LANG = 2;
+
+        constexpr size_t FUNCTION_OFFSET = 1;
+        constexpr size_t LENGTH_OFFSET = 2;
+        constexpr size_t FLAGS_OFFSET = 5;
+        constexpr size_t EXT_OFFSET = 7;
+        constexpr size_t XID_OFFSET = 10;
+        constexpr size_t LANG_LEN_OFFSET = 12;
+        constexpr size_t LANG_OFFSET = 14;
+
+        constexpr size_t MIN_LEN = 14;
+    }
+
+    namespace RESPONSE
+    {
+        constexpr size_t SIZE_ERROR = 2;
+        constexpr size_t SIZE_SERVICE = 2;
+        constexpr size_t SIZE_URL_COUNT = 2;
+        constexpr size_t SIZE_URL_ENTRY = 6;//reserved(1)+lifetime(2)+len(2)+authcount(1)
+        constexpr size_t SIZE_RESERVED = 1;
+        constexpr size_t SIZE_LIFETIME = 2;
+        constexpr size_t SIZE_URLLENGTH = 2;
+        constexpr size_t SIZE_AUTH = 1;
+
+        constexpr size_t ERROR_OFFSET = 16;
+        constexpr size_t SERVICE_LEN_OFFSET = 18;
+        constexpr size_t SERVICE_OFFSET = 20;
+        constexpr size_t URL_ENTRY_OFFSET = 18;
+        
+
+    }
+
+    namespace REQUEST
+    {
+        constexpr size_t MIN_SRVTYPE_LEN = 22;
+        constexpr size_t MIN_SRV_LEN = 26;
+
+        constexpr size_t SIZE_PRLIST = 2;
+        constexpr size_t SIZE_NAMING = 2;
+        constexpr size_t SIZE_SCOPE = 2;
+        constexpr size_t SIZE_SERVICE_TYPE = 2;
+        constexpr size_t SIZE_PREDICATE = 2;
+        constexpr size_t SIZE_SLPI = 2;
+
+        constexpr size_t PR_LEN_OFFSET = 16;
+        constexpr size_t PR_OFFSET = 18;
+        constexpr size_t SERVICE_OFFSET = 20;
+
+    }
+    
+}
