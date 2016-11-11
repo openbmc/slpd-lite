c4846e97dfa54a93fcf8e09abe8fff0afbc031d5
Ratan <ratagupt@in.ibm.com>
2016-11-11 19:12:42 +0530

    SLP Server
    This patch contains the entry point for the SLP
    and starts the SLP Server.
    
    Change-Id: I08d0f4675eeabe95127c43500a68c7819edc05be
    Signed-off-by: Ratan <ratagupt@in.ibm.com>

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include "slp_server.hpp"
+#include "sock_channel.hpp"
+#include "slp.hpp"
+#include "slp_meta.hpp"
+#include <algorithm>
+
+static int requestHandler(sd_event_source* es, int fd, uint32_t revents,
+                          void* userdata)
+{
+    uint8_t rc = SLP::SUCCESS;
+    SocketChannel channel(fd);
+    std::vector<uint8_t>recvBuff;
+    SLP::Message req;
+    std::vector<uint8_t> resp;
+
+    if (channel.read(recvBuff) != SLP::SUCCESS)
+    {
+        std::cout << "Unable to read the data" << std::endl;
+        return -1;
+    }
+    switch (recvBuff[0])
+    {
+        case SLP::VERSION_2:
+            {
+                std::cout << "Request" << std::endl;
+                //print the buffer
+                std::for_each(recvBuff.begin(), recvBuff.end(),
+                        [](uint8_t& ch) {
+                        std::cout << std::hex << ch << ' ' ;
+                        });
+
+                //Parse the buffer and construct the req object
+                rc = SLP::Parser::parseBuffer(recvBuff, req);
+                if ( !rc )
+                {
+                    //Passing the req object to handler to serve it
+                    rc = SLP::Handler::processRequest(req, resp);
+                }
+            }
+            break;
+        default:
+            rc = (uint8_t)SLP::Error::VER_NOT_SUPPORTED;
+            break;
+    }
+
+    //if there was error during Parsing of request
+    //or processing of request then handle the error.
+    if( rc )
+    {
+        std::cout << "Failure occured rc=" << rc << std::endl;
+        SLP::Handler::processError(req, resp, rc);
+    }
+
+    //print and send the response
+    std::cout << "Response" << std::endl;
+    std::for_each(resp.begin(), resp.end(),
+            [](uint8_t& ch) {
+            std::cout << std::hex << ch << ' ' ;
+            });
+
+    channel.write(resp);
+    return SLP::SUCCESS;
+}
+
+
+int main(int argc, char *argv[])
+{
+    SLP::UDP::Server svr(427,"0.0.0.0",requestHandler);
+    svr.run();
+}
---
+++ slp_server.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <systemd/sd-bus.h>
+#include <systemd/sd-daemon.h>
+#include <systemd/sd-event.h>
+
+namespace SLP
+{
+    namespace UDP
+    {
+        class Server
+        {
+          uint16_t port;
+          std::string address;
+          using CALLBACK = int (*) ( sd_event_source*, int, uint32_t, void*);
+          CALLBACK callme;
+
+          public:
+
+          Server():port(427),address("0.0.0.0"),callme(nullptr){};
+
+          Server(uint16_t port,std::string address,CALLBACK callback):
+                port(port),
+                address(address),
+                callme(callback){};
+
+          Server(const Server&) = delete;
+          Server& operator=(const Server&) = delete;
+          Server(Server&&) = delete;
+          Server& operator=(Server&&) = delete;
+
+
+          void setPort(uint16_t port) { this->port = port;};
+          void setAddress(std::string address) {this->address = address;};
+          void setCallBack(Server::CALLBACK wakeme) {this->callme = wakeme;};
+
+          int run(); 
+ 
+        };
+	}
+}
