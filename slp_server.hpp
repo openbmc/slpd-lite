ea5e6b42a75da056e35178eab508a243b071ef6c
Ratan <ratagupt@in.ibm.com>
2016-11-11 19:12:42 +0530

    SLP Server
    This patch contains the entry point for the SLP
    and starts the SLP Server.
    
    Change-Id: I08d0f4675eeabe95127c43500a68c7819edc05be
    Signed-off-by: Ratan <ratagupt@in.ibm.com>

---
+++ slp_server.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <systemd/sd-bus.h>
+#include <systemd/sd-daemon.h>
+#include <systemd/sd-event.h>
+
+namespace SLP
+{
+    namespace UDP
+    {
+        class Server
+        {
+            uint16_t port;
+            std::string address;
+            using callback = int (*)(sd_event_source*, int, uint32_t, void*);
+            callback callme;
+
+            public:
+
+            Server(): port(427), address("0.0.0.0"), callme(nullptr) {};
+
+            Server(uint16_t port, std::string address, CALLBACK callback):
+                port(port),
+                address(address),
+                callme(callback) {};
+
+            Server(const Server&) = delete;
+            Server& operator=(const Server&) = delete;
+            Server(Server&&) = delete;
+            Server& operator=(Server&&) = delete;
+
+
+            void setPort(uint16_t port)
+            {
+                this->port = port;
+            };
+            void setAddress(std::string address)
+            {
+                this->address = address;
+            };
+            void setCallBack(Server::callback wakeme)
+            {
+                this->callme = wakeme;
+            };
+
+            int run();
+
+        };
+    }
+}
